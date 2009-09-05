530a93cba2f985c8d06ada7c0423203d0551b87b
zverina@m-atelier.cz <zverina@m-atelier.cz@dca2f3e4-55bd-11de-9272-49eb38e5b1ca>
2009-09-05 14:13:04 +0000

    New repository descriptor.

--- trunk/funnytouchscreen/fts-repository/repo.sh
+++ trunk/funnytouchscreen/fts-repository/repo.sh
@@ -1,1 +1,1 @@
-echo "# Funny Touch Screen multimedia repository";echo "# version=1";ls -l | grep \\. | awk '{print $8";"$5}'
+echo "# Funny Touch Screen multimedia repository";echo "# version=1";ls -l | grep \\. | awk '{print $9";"$5}'
